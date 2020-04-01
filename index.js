f1a56c27eaf875ba513a99ac4333af8e4d814acb
piotros <piotros@wp.pl>
2020-04-02 00:09:16 +0200

    Replace index.js with command in help

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const { getConfigFromFile } = require('./lib/getConfigFromFile');
 const { sharedOptions } = require('./lib/sharedOptions');
 const { command: componentCommand } = require('./modules/component/command');
 const { command: configCommand } = require('./modules/config/command');
+const { name: packageName } = require('./package.json');
 
 yargs
+  .scriptName(packageName)
   .showHelpOnFail(false)
   .config(getConfigFromFile())
   .command(configCommand)
